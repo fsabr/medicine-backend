d5ec03e3e198397bfe1ab5d42047348bafa60257
ebby7 <abfr049@gmail.com>
2019-08-10 16:23:58 +0530

    move stuff

--- Forum/models.py
+++ Forum/models.py
@@ -3,8 +3,18 @@ from django.conf import settings
 
 # Create your models here.
 
-class Forum(models.Model):
+class Post(models.Model):
     user_type   = models.CharField(max_length=10)
     user_id     = models.IntegerField(default=0)
+    title       = models.CharField(max_length=127)
     description = models.CharField(max_length=1024)
     created_at  = models.DateTimeField(auto_now_add=True)
+    votes       = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    user_type   = models.CharField(max_length=10)
+    user_id     = models.IntegerField(default=0)
+    description = models.CharField(max_length=255)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    votes       = models.IntegerField(default=0)
+    post_id     = models.IntegerField(default=0)
--- Forum/views.py
+++ Forum/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+
+def forum(request):
+    if request.method == 'GET':
+        all_posts = Post.objects.all()
+        return render(request, 'Doctor_website/forum.html', {"posts" : all_posts})
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+def new_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+def new_comment(request):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('post_detail', pk=form.post_id)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+def post_detail(request, pk):
+    if request.method == 'GET':
+        try:
+            post = Post.objects.get(id=pk)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        comments = Comment.objects.filter(post_id=pk)
+        return render(request, 'Doctor_website/forum_post.html', {"post" : post, "comments" : comments})
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
